e88bdedb557670568bb6ae9ed113f0126015ec99
suleveer@gmail.com <suleveer@gmail.com>
2025-11-18 20:41:41 -0600

    first backend commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { AuthProvider } from "@asgardeo/auth-react";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider
+        config={ {
+            signInRedirectURL: "http://localhost:5173/",
+            signOutRedirectURL: "http://localhost:5173/",
+            clientID: "tcnir27KlgOoBuMMgqZOgIpAJbca",
+            baseUrl: "https://api.asgardeo.io/t/orgpe2w7",
+            scope: [ "openid","profile" ]
+        } }
+    >
+      <App />
+    </AuthProvider>
+  </StrictMode>,
+)
